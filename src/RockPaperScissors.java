99a0430883af64ff19611103ef2ca6e177869eb6
jeon-younghwan <younghwan314@naver.com>
2026-07-08 20:58:32 +0900

    feat: 사용자 입력 받기 기능 구현

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class RockPaperScissors {
 	public static void main(String[] args) {
@@ -13,6 +14,14 @@ public class RockPaperScissors {
 		// 랜덤 정수를 가위/바위/보로 변환
 		String[] rps = {"가위", "바위", "보"};
 		String computerChoice = rps[randomIntWithin3];
-		System.out.println(computerChoice);
+
+		// 사용자 입력 받기 위한 객체 생성
+		Scanner scanner = new Scanner(System.in);
+		// 사용자 입력 받기
+		System.out.print("가위/바위/보 중 하나를 입력하세요: ");
+		String userChoice = scanner.nextLine();
+		System.out.println("사용자 선택: " + userChoice);
+		System.out.println("컴퓨터 선택: " + computerChoice);
+		scanner.close();
 	}
 }
